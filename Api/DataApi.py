b5ae37f4ef97a5aaea05450f4f3d6dce1ec7c978
root <root@Morgoth.deadpool.com>
2018-01-15 16:35:32 +0000

    Ajout de la partie DataApi

--- Api/DataApi.py
+++ Api/DataApi.py
@@ -2,12 +2,23 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonify
+from flask_jsonpify import jsonify
 
+engine = create_engine('mysql+mysqldb://root:root@localhost/Pyviz')
+app = Flask(__name__)
+api = Api(app)
 
+class DataApi(Resource):
+    #La methode get de l'api recupere chaque entree de la table choisie
+    #retourne un fichier json
+    def get(self):
+        conn = engine.connect()
+        query = conn.execute("select commune, occurrence from postes")
+        result = {'postes': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
 
-#on renvoie les données en json
-def dataApi():
-    return 0
+api.add_resource(DataApi, '/youplaboum')
 
+if __name__ == '__main__':
+    app.run(port=1440)
 
